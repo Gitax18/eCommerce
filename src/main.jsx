5eeed4cb621e3d66a702eb19085f6c1ea8653042
Gitax18 <lusifer0418@gmail.com>
2024-09-09 21:19:20 +0530

    fix import typo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import CartContextProvider from "./feature/cart/cartContextProvider.jsx";
+import CartContextProvider from "./feature/cart/CartContextProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
