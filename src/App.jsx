a3c25e29d4d093c60858694c36983233a26633d8
Gitax18 <lusifer0418@gmail.com>
2024-09-09 12:15:20 +0530

    remove unncesarry dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Checkout from "./pages/Checkout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -53,7 +52,6 @@ const router = createBrowserRouter([
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
       <RouterProvider router={router} />
     </QueryClientProvider>
   );
